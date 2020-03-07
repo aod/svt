9815f2285cb3c9ca3fc5800c628a40c587ccbe1f
Oktay Dinler <dinler@pm.me>
2020-03-07 19:42:14 +0100

    Ignore bogo sort in all.sh script

--- scripts/all.sh
+++ scripts/all.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
-for x in `svt --algorithms`; do
+for x in `svt --algorithms`; 
+do
+    [ $x == 'bogo' ] && continue
+
     echo $x;
     sleep 1;
     svt -q -a 20 -t 3 -s $x; 
