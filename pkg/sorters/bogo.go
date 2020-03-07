62e6f1a9ba16e7fc7d3956d2e12954f848f29239
Mabel <Mattabel2002@gmail.com>
2020-03-07 09:23:37 +0000

    Changed imports and formatting

--- pkg/sorters/bogo.go
+++ pkg/sorters/bogo.go
@@ -1,7 +1,9 @@
 package sorters
 
-import "sync"
-import "math/rand"
+import (
+	"math/rand"
+	"sync"
+)
 
 func Bogo(arr []int, update chan<- int, mutex *sync.Mutex) {
 	n := len(arr)
@@ -16,8 +18,8 @@ func Bogo(arr []int, update chan<- int, mutex *sync.Mutex) {
 		})
 
 		sorted := true
-		for i := 0; i < n - 1; i++ {
-			if arr[i] > arr[i + 1] {
+		for i := 0; i < n-1; i++ {
+			if arr[i] > arr[i+1] {
 				sorted = false
 				break
 			}
